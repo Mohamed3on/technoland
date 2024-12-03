2899d4e20c2c333a9227930954eddc092bea5833
Mohamed Oun <mohamed3on@gmail.com>
2024-12-03 19:15:36 +0100

    add loader

--- app/components/CompareButton.tsx
+++ app/components/CompareButton.tsx
@@ -3,6 +3,8 @@
 import { useRouter, useSearchParams } from 'next/navigation';
 import { City } from '@/types';
 import { Button } from '@/components/ui/button';
+import { startTransition, useState } from 'react';
+import { Loader2Icon } from 'lucide-react';
 
 interface CompareButtonProps {
   city: City;
@@ -12,11 +14,15 @@ interface CompareButtonProps {
 export function CompareButton({ city, isBaseCity }: CompareButtonProps) {
   const router = useRouter();
   const searchParams = useSearchParams();
+  const [isPending, setIsPending] = useState(false);
 
   const handleCompareClick = () => {
-    const newParams = new URLSearchParams(searchParams);
-    newParams.set('baseCity', city.id);
-    router.push(`/?${newParams.toString()}`);
+    setIsPending(true);
+    startTransition(() => {
+      const newParams = new URLSearchParams(searchParams);
+      newParams.set('baseCity', city.id);
+      router.push(`/?${newParams.toString()}`);
+    });
   };
 
   if (isBaseCity) {
@@ -28,9 +34,17 @@ export function CompareButton({ city, isBaseCity }: CompareButtonProps) {
       variant={'secondary'}
       size={'sm'}
       onClick={handleCompareClick}
+      disabled={isPending}
       className='text-blue-600 hover:text-blue-900 transition-colors'
     >
-      Use {city.name} as base city
+      {isPending ? (
+        <>
+          <Loader2Icon className='animate-spin' />
+          Setting base city...
+        </>
+      ) : (
+        `Use ${city.name} as base city`
+      )}
     </Button>
   );
 }
